4a1a84635db390fdd8778feba126a2bc9c9f0fa3
David Y. Stephenson <david@davidystephenson.com>
2024-09-16 17:21:35 -0400

    Initial

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const url = require('url')
+
+function handleRequest(request, response) {
+  const uri = url.parse(request.url).pathname;
+  response.setHeader('Access-Control-Allow-Origin', '*'); /* @dev First, read about security */
+  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  response.setHeader('Access-Control-Max-Age', 2592000); // 30 days
+  response.setHeader('Access-Control-Allow-Headers', 'content-type');
+  response.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive"
+  });
+  response.write("data: " + (Math.floor(Math.random() * 1000) + 1) + "\n\n");
+  function handleOutput() {
+    const random = Math.random()
+    console.log('random:', random)
+    response.write('event: message\n');
+    response.write('data:' + random + "\n\n");
+  }
+  setInterval(handleOutput, 1000);
+}
+const port = 8000;
+function handleListen() {
+  console.log('Server is listening on port', port);
+}
+const server = http.createServer(handleRequest)
+server.listen(port, handleListen);
